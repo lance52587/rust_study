cf627d31fdd8d4360cdb5be51a12e8f80ce2d39d
lanceyliao <lanceyliao@gmail.com>
2024-03-18 10:51:35 +0800

    4

---
+++ chap18/pattern_syntax/target/debug/deps/pattern_syntax-7a1bc57737a57323.d
@@ -0,0 +1,5 @@
+/home/rust_study/chap18/pattern_syntax/target/debug/deps/pattern_syntax-7a1bc57737a57323: src/main.rs
+
+/home/rust_study/chap18/pattern_syntax/target/debug/deps/pattern_syntax-7a1bc57737a57323.d: src/main.rs
+
+src/main.rs:
---
+++ chap18/pattern_syntax/target/debug/pattern_syntax.d
@@ -0,0 +1,1 @@
+/home/rust_study/chap18/pattern_syntax/target/debug/pattern_syntax: /home/rust_study/chap18/pattern_syntax/src/main.rs
