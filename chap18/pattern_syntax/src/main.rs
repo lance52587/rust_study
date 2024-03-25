36cda9150883d42e2216d39f7810cc58a2eb5d4f
lanceyliao <lanceyliao@gmail.com>
2024-03-25 20:55:28 +0800

    pattern_syntax2

--- chap18/pattern_syntax/src/main.rs
+++ chap18/pattern_syntax/src/main.rs
@@ -54,20 +54,31 @@ fn main() {
         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
     }
 
-    let msg = Message::ChangeColor(0, 160, 255);
+    // let msg = Message::ChangeColor(0, 160, 255);
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
 
     match msg {
-        Message::Quit => {
-            println!("The Quit variant has no data to destructure.")
-        },
-        Message::Move { x, y } => {
-            println!("Move in the x direction {} and in the y direction {}", x, y)
-        },
-        Message::Write(text) => println!("Text message: {}", text),
-        Message::ChangeColor(r, g, b) => {
+        // Message::Quit => {
+        //     println!("The Quit variant has no data to destructure.")
+        // },
+        // Message::Move { x, y } => {
+        //     println!("Move in the x direction {} and in the y direction {}", x, y)
+        // },
+        // Message::Write(text) => println!("Text message: {}", text),
+        // Message::ChangeColor(r, g, b) => {
+        //     println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        // },
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
             println!("Change the color to red {}, green {}, and blue {}", r, g, b)
         },
+        Message::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change the color to hue {}, saturation {}, and value {}", h, s, v)
+        },
+        _ => (),
     }
+
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+    println!("feet: {}, inches: {}, x: {}, y: {}", feet, inches, x, y);
 }
 
 struct Point {
@@ -79,5 +90,11 @@ enum Message {
     Quit,
     Move { x: i32, y: i32 },
     Write(String),
-    ChangeColor(i32, i32, i32),
+    // ChangeColor(i32, i32, i32),
+    ChangeColor(Color),
+}
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
 }
