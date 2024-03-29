8aa5b6f48d6b0eb10c858f00b0212c5336d8fa4e
lanceyliao <lanceyliao@gmail.com>
2024-03-29 10:54:25 +0800

    pattern_syntax6

--- chap18/pattern_syntax/src/main.rs
+++ chap18/pattern_syntax/src/main.rs
@@ -159,6 +159,47 @@ fn main() {
     //         println!("Some numbers: {}", second);
     //     }
     // }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {:?}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {:?}", x, y);
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),// if运算符优先级低于|运算符
+        _ => println!("no"),
+    }
+
+    let msg = Message2::Hello { id: 5 };
+
+    match msg {
+        Message2::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message2::Hello { id: 10..=12 } => println!("Found an id in another range"),
+        Message2::Hello { id } => println!("Found some other id: {}", id),
+    }
+}
+
+enum Message2 {
+    Hello { id: i32 },
 }
 
 struct Point3D {
