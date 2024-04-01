7d86bf4c838352bb5e05939835cb84b950ff26d4
lanceyliao <lanceyliao@gmail.com>
2024-04-01 13:05:57 +0800

    unsafe1

---
+++ chap19/unsafe1/target/debug/deps/unsafe1-e5da4e17515f26cb.d
@@ -0,0 +1,5 @@
+/home/rust_study/chap19/unsafe1/target/debug/deps/unsafe1-e5da4e17515f26cb: src/main.rs
+
+/home/rust_study/chap19/unsafe1/target/debug/deps/unsafe1-e5da4e17515f26cb.d: src/main.rs
+
+src/main.rs:
---
+++ chap19/unsafe1/target/debug/unsafe1.d
@@ -0,0 +1,1 @@
+/home/rust_study/chap19/unsafe1/target/debug/unsafe1: /home/rust_study/chap19/unsafe1/src/main.rs
