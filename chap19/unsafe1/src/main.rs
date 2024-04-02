b633c6dd3f5d9d1e55f8b58ef744f842bcd35d66
lanceyliao <lanceyliao@gmail.com>
2024-04-02 09:56:12 +0800

    unsafe2

--- chap19/unsafe1/src/main.rs
+++ chap19/unsafe1/src/main.rs
@@ -12,4 +12,29 @@ fn main() {
         println!("r2 is: {}", *r2);
         // println!("r is: {}", *r);
     }
+
+    unsafe fn dangerous(){}
+    
+    unsafe {
+        dangerous();
+    }
+
+    // dangerous();// requires unsafe function or block
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    let r = &mut v[..];
+
+    let (a, b) = r.split_at_mut(3);
+
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+}
+
+fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]){
+    let len = slice.len();
+
+    assert!(mid <= len);
+
+    (&mut slice[..mid], &mut slice[mid..])
 }
