f564073e72530ca183561fdc7d0f0ba8a0d869af
lanceyliao <lanceyliao@gmail.com>
2024-04-02 10:56:16 +0800

    unsafe3

--- chap19/unsafe1/src/main.rs
+++ chap19/unsafe1/src/main.rs
@@ -29,12 +29,26 @@ fn main() {
 
     assert_eq!(a, &mut [1, 2, 3]);
     assert_eq!(b, &mut [4, 5, 6]);
+
+    let address = 0x01234usize;
+    let r = address as *mut i32;
+
+    let slice : &[i32] = unsafe{
+        slice::from_raw_parts_mut(r, 10000)
+    };
 }
 
+use std::slice;
 fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]){
     let len = slice.len();
+    let ptr = slice.as_mut_ptr();
 
     assert!(mid <= len);
 
-    (&mut slice[..mid], &mut slice[mid..])
+    // (&mut slice[..mid], &mut slice[mid..])
+    unsafe{
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.offset(mid as isize), len - mid))
+    }
 }
