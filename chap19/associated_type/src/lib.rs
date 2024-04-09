42e45f4a4e7c71a6638707582b52cb39795c518c
lanceyliao <lanceyliao@gmail.com>
2024-04-09 09:42:07 +0800

    operator overloading

--- chap19/associated_type/src/lib.rs
+++ chap19/associated_type/src/lib.rs
@@ -1,7 +1,20 @@
-impl Iterator for Counter{
-    type Item = u32;
+// impl Iterator for Counter{
+//     type Item = u32;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        // --略
+//     fn next(&mut self) -> Option<Self::Item> {
+//         // --略
+//     }
+// }
+
+use std::ops::Add;
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {//指定impl Add<Meters>来设置RHS类型参数的值，而没有使用默认的Self
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
     }
 }
--- chap19/associated_type/src/main.rs
+++ chap19/associated_type/src/main.rs
@@ -3,6 +3,30 @@ pub trait Iterator {
     fn next(&mut self) -> Option<Self::Item>;
 }
 
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// trait Add<RHS=Self> {
+//     type Output;
+//     fn add(self, rhs: RHS) -> Self::Output;
+// }
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(Point { x: 1, y: 0 } + Point { x: 2, y: 3 }, Point { x: 3, y: 3 });
 }
