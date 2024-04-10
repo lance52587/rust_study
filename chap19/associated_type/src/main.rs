7a2e1ca72ccea5e21a4cf7f1e509fb1869f8b28f
lanceyliao <lanceyliao@gmail.com>
2024-04-10 09:17:47 +0800

    fully qualified syntax

--- chap19/associated_type/src/main.rs
+++ chap19/associated_type/src/main.rs
@@ -27,6 +27,62 @@ impl Add for Point {
     }
 }
 
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
 fn main() {
+    println!("A baby dog is called a {}", Dog::baby_name());
+    // println!("A baby dog is called a {}", Animal::baby_name());
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+
+    let person = Human;
+    Pilot::fly(&person);//This is your captain speaking.
+    Wizard::fly(&person);//Up!
+    // person.fly();//*waving arms furiously*
+    Human::fly(&person);//*waving arms furiously*
+
     assert_eq!(Point { x: 1, y: 0 } + Point { x: 2, y: 3 }, Point { x: 3, y: 3 });
 }
