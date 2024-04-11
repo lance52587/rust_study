6ad8a000c782f4d4ea35e0f0343572972d949b61
lanceyliao <lanceyliao@gmail.com>
2024-04-11 09:35:59 +0800

    super trait

--- chap19/associated_type/src/main.rs
+++ chap19/associated_type/src/main.rs
@@ -73,7 +73,32 @@ impl Animal for Dog {
     }
 }
 
+use std::fmt;
+
+trait OutlinePrint: fmt::Display{
+    fn outline_print(&self){
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+impl OutlinePrint for Point{}// the trait `std::fmt::Display` is not implemented for `Point`
+
+impl fmt::Display for Point{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        write!(f, "({}, {})", self.x, self.y)// write表示将格式化的字符串写入到f中，定义了outline_print中调用的to_string
+    }
+}
+
 fn main() {
+    let p = Point { x: 1, y: 2 };
+    p.outline_print();
+
     println!("A baby dog is called a {}", Dog::baby_name());
     // println!("A baby dog is called a {}", Animal::baby_name());
     println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
