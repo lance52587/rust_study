c9f72949cadb77d769702b1818f92200754b2c1a
lanceyliao <lanceyliao@gmail.com>
2024-04-14 11:48:30 +0800

    type alias

--- chap19/AdvancedType/src/main.rs
+++ chap19/AdvancedType/src/main.rs
@@ -13,6 +13,23 @@ fn returns_long_type() -> Thunk {
     Box::new(|| ())
 }
 
+use std::io::Error;
+use std::fmt;
+
+type Result<T> = std::result::Result<T, std::io::Error>;
+
+pub trait Write {
+    // fn write(&mut self, buf: &[u8]) -> Result<usize, Error>;
+    fn write(&mut self, buf: &[u8]) -> Result<usize>;
+    // fn flush(&mut self) -> Result<(), Error>;
+    fn flush(&mut self) -> Result<()>;
+
+    // fn write_all(&mut self, buf: &[u8]) -> Result<(), Error>;
+    fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+    // fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<(), Error>;
+    fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
+}
+
 fn main() {
     type Kilometers = i32;
 
