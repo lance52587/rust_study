ce4ea3dde63fb0362aac271ffecacd37e589e444
lanceyliao <lanceyliao@gmail.com>
2024-04-17 12:47:21 +0800

    return closure

--- chap19/AdvancedTypeAndClosure/src/main.rs
+++ chap19/AdvancedTypeAndClosure/src/main.rs
@@ -11,6 +11,12 @@ enum Status {
     Stop,
 }
 
+// fn returns_closure() -> Fn(i32) -> i32 {// does not have a size known at compile-time
+    // |x| x + 1
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
 fn main() {
     let list_of_statuses: Vec<Status> = (0u32..20)// 0..20 is a range of numbers from 0 to 19
         .map(Status::Value)
