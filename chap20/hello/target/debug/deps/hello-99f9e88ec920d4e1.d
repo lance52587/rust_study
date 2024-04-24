9bdda3f19f07707b58fd45f5a5eb85f2ab74a6d6
lanceyliao <lanceyliao@gmail.com>
2024-04-24 10:54:26 +0800

    tcp stream

---
+++ chap20/hello/target/debug/deps/hello-99f9e88ec920d4e1.d
@@ -0,0 +1,5 @@
+/home/rust_study/chap20/hello/target/debug/deps/hello-99f9e88ec920d4e1: src/main.rs
+
+/home/rust_study/chap20/hello/target/debug/deps/hello-99f9e88ec920d4e1.d: src/main.rs
+
+src/main.rs:
---
+++ chap20/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/rust_study/chap20/hello/target/debug/hello: /home/rust_study/chap20/hello/src/main.rs
