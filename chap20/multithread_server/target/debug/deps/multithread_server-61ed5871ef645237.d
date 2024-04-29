57232de73ce4dfe1437ad9eaac1f5927e54c0e96
lanceyliao <lanceyliao@gmail.com>
2024-04-29 10:04:13 +0800

    thread1

---
+++ chap20/multithread_server/target/debug/deps/multithread_server-61ed5871ef645237.d
@@ -0,0 +1,5 @@
+/home/rust_study/chap20/multithread_server/target/debug/deps/multithread_server-61ed5871ef645237: src/main.rs
+
+/home/rust_study/chap20/multithread_server/target/debug/deps/multithread_server-61ed5871ef645237.d: src/main.rs
+
+src/main.rs:
---
+++ chap20/multithread_server/target/debug/multithread_server.d
@@ -0,0 +1,1 @@
+/home/rust_study/chap20/multithread_server/target/debug/multithread_server: /home/rust_study/chap20/multithread_server/src/main.rs
