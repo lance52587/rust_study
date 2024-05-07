f73822858cd970f2b5f58d7106010cf3af709abb
lanceyliao <lanceyliao@gmail.com>
2024-05-07 11:21:18 +0800

    thread pool workers

--- chap20/multithread_server/src/lib.rs
+++ chap20/multithread_server/src/lib.rs
@@ -2,7 +2,8 @@ use std::thread;
 
 // pub struct ThreadPool;
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    // threads: Vec<thread::JoinHandle<()>>,
+    threads: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -20,11 +21,14 @@ impl ThreadPool {
     // 返回Result类型，而不是直接panic
     // pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
 
-        let mut threads = Vec::with_capacity(size);
+        // let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
         // 与Vec::new有些类似，但区别在于with_capacity会为动态数组预分配出指定的空间。
         // 在知晓存储大小的前提下预先分配存储空间要比使用Vec::new在插入时动态扩展大小更有效率一些。
-        for _ in 0..size {
+        // for _ in 0..size {
+        for id in 0..size {
             // 创建线程并将他们存储至动态数组中
+            workers.push(Worker::new(id));
         }
         ThreadPool {
             threads
@@ -43,5 +47,20 @@ impl ThreadPool {
         // FnOnce后的()意味着传入的闭包既没有参数，也不返回结果。
         // 就像函数定义一样，我们可以省略签名中的返回值，但却不能省略函数名后的圆括号，即便括号中没有任何参数。
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+        Worker {
+            id,
+            thread,
+        }
     }
 }
