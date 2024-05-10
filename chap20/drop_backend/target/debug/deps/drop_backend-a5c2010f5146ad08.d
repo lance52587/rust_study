4eb737b44f7465386138e11e264bc52d57dd9b25
lanceyliao <lanceyliao@gmail.com>
2024-05-10 09:24:02 +0800

    为ThreadPool实现Drop trait

---
+++ chap20/drop_backend/target/debug/deps/drop_backend-a5c2010f5146ad08.d
@@ -0,0 +1,5 @@
+/home/rust_study/chap20/drop_backend/target/debug/deps/libdrop_backend-a5c2010f5146ad08.rmeta: src/lib.rs
+
+/home/rust_study/chap20/drop_backend/target/debug/deps/drop_backend-a5c2010f5146ad08.d: src/lib.rs
+
+src/lib.rs:
---
+++ chap20/drop_backend/target/debug/deps/main-b7665b5126c63394.d
@@ -0,0 +1,5 @@
+/home/rust_study/chap20/drop_backend/target/debug/deps/libmain-b7665b5126c63394.rmeta: src/bin/main.rs
+
+/home/rust_study/chap20/drop_backend/target/debug/deps/main-b7665b5126c63394.d: src/bin/main.rs
+
+src/bin/main.rs:
